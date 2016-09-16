bb5fd1c1f33d8a59cd834032d384fdd24099439c
John Brilla <jbrilla17@gmail.com>
2016-09-16 16:38:13 -0400

    adding strong params

--- app.rb
+++ app.rb
@@ -13,24 +13,36 @@ end
 
 # GET FOR USER
 post '/user' do
-	chatter.write_to_csv "STATUS", "#{params[:user].strip.upcase} HAS JOINED THE CHANNEL"
+	chatter.write_to_csv "STATUS", "#{user_strong_params.upcase} HAS JOINED THE CHANNEL"
 
-	redirect "/chat/#{params[:user].strip}"
+	redirect "/chat/#{user_strong_params}"
 end
 
 # CHAT per User
 get '/chat/:user' do
-	@user = params[:user].strip
+	@user = user_strong_params
 	@chat = chatter.parse_csv
-
 	erb :chat
 end
 
 # CHAT POST METHOD - WITH USER
 post '/:user/message' do
 	# Executing chat
-	chatter.write_to_csv params[:user], params[:message].strip
+	chatter.write_to_csv user_strong_params, message_strong_params
 
-	redirect "/chat/#{params[:user]}"
+	redirect "/chat/#{user_strong_params}"
+end
+
+private
+def user_strong_params
+    html_safe(params[:user]).strip
+end
+
+def message_strong_params
+	html_safe(params[:message]).strip
+end
+ 
+def html_safe(text)
+  Rack::Utils.escape_html(text)
 end
 
